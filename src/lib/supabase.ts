87d99e0c142ecfa575db1a3016d7b49b0647b31b
Megan Kandiah <curvescurlsandclothes@gmail.com>
2026-05-27 08:05:26 -0500

    Fix production Supabase URL

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,10 @@
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AppState, Platform } from "react-native";
-import { createClient, processLock } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://axaqrmecztqrsqpennau.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4YXFybWVjenRxcnNxcGVubmF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4OTI4MDgsImV4cCI6MjA5NDQ2ODgwOH0.qhK-SNykhx6hYKh9E_fQ3YKl2lYWZPo-QTtNG8D7ao8";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
@@ -12,16 +12,12 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
-    lock: processLock,
   },
 });
 
 if (Platform.OS !== "web") {
   AppState.addEventListener("change", (state) => {
-    if (state === "active") {
-      supabase.auth.startAutoRefresh();
-    } else {
-      supabase.auth.stopAutoRefresh();
-    }
+    if (state === "active") supabase.auth.startAutoRefresh();
+    else supabase.auth.stopAutoRefresh();
   });
 }
